fac15541e6cddb0d7e1854214b915d335fb947d6
Divjot Virdi <dvirdi01@students.cs.ubc.ca>
2022-11-19 13:52:37 -0800

    Added base for jtable (ignore colors)

--- src/main/CalorieTracker.java
+++ src/main/CalorieTracker.java
@@ -1,6 +1,8 @@
 package main;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
@@ -25,6 +27,12 @@ public class CalorieTracker {
 
     //TextFields
     JTextField itemName = new JTextField();
+
+    //JTables
+    JTable loggingTable;
+    DefaultTableModel loggingTableModel;
+    DefaultTableCellRenderer loggingCellRenderer;
+    String[] logRowComponents;
 
     public CalorieTracker() {
         setUpFrame();
@@ -101,11 +109,75 @@ public class CalorieTracker {
         mainPanelLabel.setForeground(Color.BLACK);
         mainPanelLabel.setFont(new Font("Verdana", Font. BOLD, 25));
         mainPanel.add(mainPanelLabel);
+        displayLeftPanel();
+        //displayButtons();
 
         topPanel.setVisible(true);
         mainFrame.add(topPanel, borderLayout.NORTH);
         mainPanel.setVisible(true);
+        displayLogTable();
         mainFrame.add(mainPanel, borderLayout.CENTER);
+    }
+
+    private void displayLeftPanel() {
+        leftPanel.setLayout(new BoxLayout(leftPanel, BoxLayout.Y_AXIS));
+        leftPanel.setBackground(new Color(142, 239, 138));
+        leftPanel.setPreferredSize(new Dimension(200, 50));
+
+        // create labels
+        JLabel dateLabel = new JLabel("Date(D/M/Y)");
+        JLabel foodLabel = new JLabel("Food");
+
+        // add labels
+        leftPanel.add(Box.createRigidArea(new Dimension(15, 10)));
+        leftPanel.add(dateLabel);
+        leftPanel.add(Box.createRigidArea(new Dimension(15, 10)));
+        leftPanel.add(foodLabel);
+
+        //-----------------------------------------------------------------
+        leftPanel.setVisible(true);
+        mainFrame.add(leftPanel, borderLayout.WEST);
+    }
+
+    private void displayButtons() {
+        performAddButtonTask();
+        performModifyButtonTask();
+        performDeleteButtonTask();
+    }
+
+    private void performDeleteButtonTask() {
+    }
+
+    private void performModifyButtonTask() {
+    }
+
+    private void performAddButtonTask() {
+    }
+
+
+
+
+
+    //MODIFIES: this
+    //EFFECTS: creates a call log which is displayed upon button click
+    private void displayLogTable() {
+        loggingTable = new JTable();
+        loggingTableModel = new DefaultTableModel();
+        loggingTable.setModel(loggingTableModel);
+
+        loggingTableModel.addColumn("Date");
+        loggingTableModel.addColumn("Food");
+//      loggingTable.getColumnModel().getColumn(0).setPreferredWidth(1);
+
+        loggingTable.setRowSelectionAllowed(true);
+        loggingTable.setCellSelectionEnabled(true);
+
+        JScrollPane scrollPane = new JScrollPane(loggingTable);
+        loggingTable.setBackground(new Color(26, 175, 119, 255));
+        loggingTable.setFillsViewportHeight(true);
+        scrollPane.setVisible(true);
+        mainPanel.add(scrollPane);
+
     }
 
 }
