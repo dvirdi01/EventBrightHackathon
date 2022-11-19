c17b9b50fe37397c1f9bbd1003e0f270bcced11c
Divjot Virdi <dvirdi01@students.cs.ubc.ca>
2022-11-19 11:42:01 -0800

    added title on panel

--- src/main/CalorieTracker.java
+++ src/main/CalorieTracker.java
@@ -7,8 +7,8 @@ import java.awt.event.WindowListener;
 
 public class CalorieTracker {
 
-    static final int FRAME_WIDTH = 800;
-    static final int FRAME_HEIGHT = 600;
+    static final int FRAME_WIDTH = 1000;
+    static final int FRAME_HEIGHT = 900;
     JFrame mainFrame = new JFrame();
     BorderLayout borderLayout = new BorderLayout();
 
@@ -33,11 +33,11 @@ public class CalorieTracker {
 
     private void setUpFrame() {
         mainFrame = new JFrame();
-        mainFrame.setTitle("Telephone Hut");
+        mainFrame.setTitle("Calorie Tracker");
         mainFrame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         mainFrame.setLayout(borderLayout);
-        mainFrame.getContentPane().setBackground(new Color(203, 206, 227, 255));
+        mainFrame.getContentPane().setBackground(new Color(255, 255, 255, 255));
         mainFrame.setResizable(true);
         mainFrame.addWindowListener(new WindowListener() {
 
@@ -79,7 +79,21 @@ public class CalorieTracker {
     }
 
     private void setUpPanel() {
+        topPanel = new JPanel();
+        topPanel.setBackground(new Color(6, 54, 2));
+        topPanel.setPreferredSize(new Dimension(50, 100));
 
+        JLabel topPanelLabel = new JLabel();
+        topPanelLabel.setText("Calories Tracker");
+        topPanelLabel.setForeground(Color.WHITE);
+        topPanelLabel.setFont(new Font("Verdana", Font. BOLD, 25));
+
+        topPanel.add(Box.createRigidArea(new Dimension(70, 50)));
+        topPanel.add(topPanelLabel);
+
+
+        topPanel.setVisible(true);
+        mainFrame.add(topPanel, borderLayout.NORTH);
     }
 
 }
--- src/main/CalorieTrackerUI.java
+++ src/main/CalorieTrackerUI.java
@@ -3,6 +3,7 @@ package main;
 public class CalorieTrackerUI {
     public static void main(String[] args) {
         CalorieTracker ct = new CalorieTracker();
+        ct.mainFrame.setVisible(true);
     }
 
 }
